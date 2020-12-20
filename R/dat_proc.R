c87922b301a90a3e4d5beaede13d1fbeb6923922
fawda123 <mbafs2012@gmail.com>
2020-12-20 12:40:48 -0500

    hydrology load added back to 2012

--- R/dat_proc.R
+++ R/dat_proc.R
@@ -17,15 +17,34 @@ ips8519 <- read_sas('~/Desktop/TBEP/LoadingCodes&Datasets1719_toTBEP_20201117/To
 nps8519 <- read_sas('~/Desktop/TBEP/LoadingCodes&Datasets1719_toTBEP_20201117/TotalLoads1719/TotalLoads2019/nps_8519.sas7bdat')
 gws8519 <- read_sas('~/Desktop/TBEP/LoadingCodes&Datasets1719_toTBEP_20201117/TotalLoads1719/TotalLoads2019/gws_8519.sas7bdat')
 
-tndat <- bind_rows(ad8519, dps8519, ips8519, nps8519, gws8519) %>% 
-  left_join(segid, by = 'BAY_SEG')
+# TN is in tons / yr, h2o is in million m3 / yr
+dat <- bind_rows(ad8519, dps8519, ips8519, nps8519, gws8519) %>% 
+  left_join(segid, by = 'BAY_SEG') %>% 
+  rename(
+    hy_load = h2oload10e6m3, 
+    tn_load = TN_tons
+  )  
 
 # totals
-tots <- tndat%>% 
+tots <- dat %>% 
   group_by(YEAR, SOURCE) %>% 
-  summarise(TN_tons = sum(TN_tons, na.rm = T), .groups = 'drop') %>% 
+  summarise(
+    tn_load = sum(tn_load),
+    hy_load = sum(hy_load),
+    .groups = 'drop'
+    ) %>% 
   mutate(bay_segment = 'All Segments (- N. BCB)')
 
-tndat <- bind_rows(tots, tndat)
+dat <- bind_rows(tots, dat)
+
+# tn data only
+tndat <- dat %>% 
+  select(SOURCE, YEAR, tn_load, bay_segment)
+
+# hy dat only
+hydat <- dat %>% 
+  select(SOURCE, YEAR, hy_load, bay_segment) %>% 
+  filter(!is.na(hy_load))
 
 save(tndat, file = 'data/tndat.RData', compress = 'xz')
+save(hydat, file = 'data/hydat.RData', compress = 'xz')
