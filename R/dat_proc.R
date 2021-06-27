fcf9098049daa0e9fcaaee0226c54047f81c90ad
fawda123 <mbafs2012@gmail.com>
2021-06-27 14:50:18 -0400

    2020 annual summary added

--- R/dat_proc.R
+++ R/dat_proc.R
@@ -11,22 +11,57 @@ segid <- tibble(
 # sas files ---------------------------------------------------------------
 
 # 85 - 19
-ad8519 <- read_sas('~/Desktop/TBEP/LoadingCodes&Datasets1719_toTBEP_20201117/TotalLoads1719/TotalLoads2019/ad_8519.sas7bdat')
-dps8519 <- read_sas('~/Desktop/TBEP/LoadingCodes&Datasets1719_toTBEP_20201117/TotalLoads1719/TotalLoads2019/dps_8519.sas7bdat')
-ips8519 <- read_sas('~/Desktop/TBEP/LoadingCodes&Datasets1719_toTBEP_20201117/TotalLoads1719/TotalLoads2019/ips_ml_8519.sas7bdat')
-nps8519 <- read_sas('~/Desktop/TBEP/LoadingCodes&Datasets1719_toTBEP_20201117/TotalLoads1719/TotalLoads2019/nps_8519.sas7bdat')
-gws8519 <- read_sas('~/Desktop/TBEP/LoadingCodes&Datasets1719_toTBEP_20201117/TotalLoads1719/TotalLoads2019/gws_8519.sas7bdat')
+# from here C:\Users\mbeck\Desktop\TBEP\Janicki loadings\LoadingCodes&Datasets1719_toTBEP_20201117\TotalLoads1719\TotalLoads2019
+ad8519 <- read_sas('data/raw/ad_8519.sas7bdat')
+dps8519 <- read_sas('data/raw/dps_8519.sas7bdat')
+ips8519 <- read_sas('data/raw/ips_ml_8519.sas7bdat')
+nps8519 <- read_sas('data/raw/nps_8519.sas7bdat')
+gws8519 <- read_sas('data/raw/gws_8519.sas7bdat')
 
 # h2o load data prior to 2012, no source info
-hyolddat <- read_sas('~/Desktop/TBEP/LoadingCodes&Datasets1719_toTBEP_20201117/TotalLoads1719/TotalLoads2019/h2oannseg8511.sas7bdat')
+hyolddat <- read_sas('data/raw/h2oannseg8511.sas7bdat')
+
+# 2020 data as month, summarized to year
+lds20 <- read_sas('data/raw/totn2020_monthsegsource_20210617.sas7bdat') %>% 
+  mutate(
+    bay_seg = case_when(
+      bay_seg %in% c(5, 6, 7, 55) ~ 5567, 
+      T ~ bay_seg
+    ), 
+    source = case_when(
+      source == 'DP' ~ 'DPS', 
+      source == 'GW' ~ 'GWS', 
+      source == 'IP' ~ 'IPS', 
+      source == 'NP' ~ 'NPS', 
+      T ~ source
+    )
+  ) %>% 
+  filter(source %in% c('AD', 'DPS', 'GWS', 'IPS', 'NPS')) %>% 
+  group_by(bay_seg, year, source) %>% 
+  summarise(
+    tn_load = sum(tnload, na.rm = T),
+    hy_load = sum(h2oload, na.rm = T),
+    .groups = 'drop'
+  ) %>% 
+  mutate(
+    tn_load = tn_load / 1e3, # convert to tons?
+    hy_load = hy_load / 1e6 # convert m3/yr to mill m3/yr
+  ) %>% 
+  rename(
+    BAY_SEG = bay_seg, 
+    SOURCE = source, 
+    YEAR = year
+  ) %>% 
+  select(SOURCE, BAY_SEG, YEAR, tn_load, hy_load)
 
 # TN is in tons / yr, h2o is in million m3 / yr
 dat <- bind_rows(ad8519, dps8519, ips8519, nps8519, gws8519) %>% 
-  left_join(segid, by = 'BAY_SEG') %>% 
   rename(
     hy_load = h2oload10e6m3, 
     tn_load = TN_tons
-  )  
+  ) %>% 
+  bind_rows(lds20) %>% 
+  left_join(segid, by = 'BAY_SEG') 
 
 # totals
 tots <- dat %>% 
@@ -56,14 +91,14 @@ save(tndat, file = 'data/tndat.RData', compress = 'xz')
 # tn load, sum source within bay segment
 tntots <- tndat %>% 
   group_by(bay_segment, YEAR) %>% 
-  summarise(tn_load = sum(tn_load), .groups = 'drop')
+  summarise(tn_load = sum(tn_load, na.rm = T), .groups = 'drop')
 
-# hy dat only, 2012 to 2019, sum across source
+# hy dat only, 2012 to 2020, sum across source
 hytots <- dat %>% 
   select(SOURCE, YEAR, hy_load, bay_segment) %>% 
   filter(!is.na(hy_load)) %>% 
   group_by(bay_segment, YEAR) %>% 
-  summarise(hy_load = sum(hy_load), .groups = 'drop')
+  summarise(hy_load = sum(hy_load, na.rm = T), .groups = 'drop')
 
 # hy dat prior to 2012
 hyoldtots <- hyolddat %>% 
@@ -79,7 +114,7 @@ hyoldtots <- hyolddat %>%
 hyoldallseg <- hyoldtots %>% 
   group_by(YEAR) %>% 
   summarise(
-    hy_load = sum(hy_load),
+    hy_load = sum(hy_load, na.rm = T),
     .groups = 'drop'
   ) %>% 
   mutate(bay_segment = 'All Segments (- N. BCB)')
