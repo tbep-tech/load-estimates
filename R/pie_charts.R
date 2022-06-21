60867f6c88a9b3fa4d355513e741b17c715e0fd0
fawda123 <mbafs2012@gmail.com>
2022-06-21 15:55:42 -0400

    summary plot from kg/yr to tons/yr fix #11

--- R/pie_charts.R
+++ R/pie_charts.R
@@ -31,26 +31,36 @@ dev.off()
 
 # plotrix 3d --------------------------------------------------------------
 
-srcs <- c('AD', 'DPS', 'IPS', 'NPS', 'GWS')
-labs <- c('Atmospheric Deposition', 'Domestic Point Source', 'Industrial Point Sources', 'Nonpoint Sources', 'Groundwater & Springs')
-cols <- c('AD' = I('#33CC33'), 'DPS' = I('#00B0F0'), 'IPS' = I('darkred'), 'NPS' = I('#FFFF99'), 'GWS' = I('#EB641B'))
+##
+# historic estimates
+labs <- c('Atmospheric Deposition', 'Fertilizer Losses', 'Point Sources', 'Nonpoint Sources', 'Groundwater & Springs')
+cols <- c('#33CC33', '#00B0F0', 'darkred', '#FFFF99', '#EB641B')
 names(cols) <- labs
 yrlabs <- c('1970s', '1985-1989', '1990s', '2000s', '2010s')
 
 # from TBEP #04-94
-# totals are kg/yr
+# totals are kg/yr, conveted to tons/yr
 historic <- tibble(
-    source = c('Atmospheric Deposition', 'Domestic Point Source', 'Industrial Point Sources', 'Nonpoint Sources', 'Groundwater & Springs'),
-    yearcat = '1970s',
-    tot = c(967059.21, 443613.465, 5418616.005, 2150028.45, 5443.11), 
-    nyr = 1
-  ) %>% 
+  source = labs,
+  yearcat = '1970s',
+  tot = c(967059.21, 443613.465, 5418616.005, 2150028.45, 5443.11), 
+  nyr = 1
+) %>% 
   mutate(
     yearcat = factor(yearcat, levels = yrlabs),
+    tot = tot / 907.185,
     per = 100 * tot / sum(tot), 
-    kgyr = sum(tot) / nyr / 1e6
+    tonyr = sum(tot) / nyr, 
+    cols
   )
-  
+
+##
+# current estimates, adding historical
+
+srcs <- c('AD', 'DPS', 'IPS', 'NPS', 'GWS')
+labs <- c('Atmospheric Deposition', 'Domestic Point Sources', 'Industrial Point Sources', 'Nonpoint Sources', 'Groundwater & Springs')
+cols <- c('#33CC33', '#E6B9B8', '#C0504D', '#FFFF99', '#EB641B')
+
 # load in tons, convert to kg
 toplo <- tnanndat %>% 
   filter(bay_segment %in% 'All Segments (- N. BCB)') %>% 
@@ -59,10 +69,11 @@ toplo <- tnanndat %>%
                   breaks = c(-Inf, 1970, 1990, 2000, 2010, Inf), 
                   labels = yrlabs, 
                   right = F) ,
-    source = factor(source, levels = srcs, labels = labs)
+    source = factor(source, levels = srcs, labels = labs), 
+    cols = as.character(factor(source, levels = labs, labels = cols))
   ) %>% 
-  select(source, year, yearcat, tn_load) %>% 
-  group_by(source, yearcat) %>% 
+  select(source, year, yearcat, tn_load, cols) %>% 
+  group_by(source, yearcat, cols) %>% 
   summarise(
     tot = sum(tn_load), 
     nyr = length(unique(year)), 
@@ -71,45 +82,45 @@ toplo <- tnanndat %>%
   group_by(yearcat) %>% 
   mutate(
     per = 100 * tot / sum(tot), 
-    kgyr = 907.185 * sum(tot) / nyr, 
-    kgyr = kgyr / 1e6
+    tonyr = sum(tot) / nyr
   ) %>% 
   ungroup %>% 
   bind_rows(historic) %>% 
   mutate(
-    rad = rescale(kgyr, newrange = c(0.5, 1.4)), 
+    rad = rescale(tonyr, newrange = c(0.5, 1)), 
     lab = gsub('^0$', '< 1', round(per, 0)), 
     lab = paste0(lab, '%'), 
-    ttl = paste0(yearcat, ': ', round(kgyr, 1), ' mill kg/yr')
+    ttl = paste0(yearcat, ': ', formatC(round(tonyr, 0), big.mark = ',', format = 'd'), ' tons/yr')
   )
 
-png(here('figs/3dload_pies.png'), height = 3, width = 9, res = 500, units = 'in')
+png(here('figs/3dload_pies.png'), height = 8, width = 8, units = 'in', res = 500, family = fml)
 
-par(mfrow = c(1, 2), mar = c(1, 0, 1, 0), xpd = NA)
+par(mfrow = c(2, 1), mar = c(1, 0, 1, 0), xpd = NA)
 
 for(i in c('1970s', '2010s')){
-
+  
   tmp <- toplo %>%
     filter(yearcat == i)
-
+  
   rad <- unique(tmp$rad)
   ttl <- unique(tmp$ttl)
-
+  cols <- tmp$cols
+  src <- tmp$source
+  
   p <- pie3D(tmp$tot, mar = c(0, 6, 0, 6),
              col = cols,
              radius = rad,
              theta = 0.9,
              explode = 0.1,
-             height = 0.075,
+             height = 0.06,
              shade = 0.8)
-  pie3D.labels(p, labels = tmp$lab, theta = 0.9, labelrad = rad + 0.4, labelcex = 1)
-  title(ttl)
-
+  pie3D.labels(p, labels = tmp$lab, theta = 0.9, labelrad = rad + 0.25, labelcex = 1)
+  title(ttl, line = -1)
+  
+  ord <- matrix(1:6, nrow = 2, ncol = 3, byrow = T)
+  legend(x = -1.65, y = -0.65, ncol = 3, legend = na.omit(rev(src)[ord]), xpd = NA, fill = na.omit(rev(cols)[ord]), box.col = NA)
+  
 }
 
-ord <- matrix(1:6, nrow = 2, ncol = 3, byrow = T)
-legend(x = -5.5, y = -1, ncol = 3, legend = na.omit(rev(labs)[ord]), xpd = NA, fill = na.omit(rev(cols)[ord]), box.col = NA)
-
 dev.off()
-
 
