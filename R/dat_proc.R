acebe5c8a58b37ea43a24bfaaaab3343bc372dc3
fawda123 <mbafs2012@gmail.com>
2023-02-14 16:10:18 -0500

    update mosentdat

--- R/dat_proc.R
+++ R/dat_proc.R
@@ -237,8 +237,8 @@ save(mosdat, file = here('data/mosdat.RData'), version = 2)
 
 # all monthly tn, tp, tss, bod estimates by entity ------------------------
 
-# source here: T:\03_BOARDS_COMMITTEES\05_TBNMC\2022_RA_Update\01_FUNDING_OUT\DELIVERABLES\TO-8\2017-2020Annual&MonthlyLoadDatasets
-mosentdat <- read_sas(here('data/raw/monthly1720entityloaddataset.sas7bdat')) %>% 
+# original at T:/03_BOARDS_COMMITTEES/05_TBNMC/2022_RA_Update/01_FUNDING_OUT/DELIVERABLES/TO-9/datastick_deliverables/2017-2021Annual&MonthlyLoadDatasets/MakeMonthAnnDatasets/Monthly/monthly1721entityloaddataset.sas7bdat
+mosentdat <- read_sas(here('data/raw/monthly1721entityloaddataset.sas7bdat')) %>% 
   select(entity, year = YEAR, month = MONTH, source, tnloadtons, tploadtons, tssloadtons, bodloadtons) %>% 
   mutate(
     source = case_when(
